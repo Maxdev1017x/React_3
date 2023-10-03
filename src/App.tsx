8856e11f736bac0161e3a7cdc8a2c35e8a193708
Maxdev1017x <Thelastmc1@gmail.com>
2023-10-03 17:29:41 -0300

    commit inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef, useMemo, useCallback } from 'react'
 import './App.css'
 
 function App() {
+
+  const inputRef = useRef<HTMLInputElement>(null)
+  const firstRender = useRef(true)
   const [input, setInput] = useState("")
   const [tasks, setTasks] = useState<string[]>([])
 
@@ -10,7 +13,41 @@ function App() {
     task: ''
   })
 
-  function handleRegister() {
+  useEffect(() => {
+    const tarefaSalvas = localStorage.getItem("@maxlist")
+  
+    if(tarefaSalvas) {
+      setTasks(JSON.parse(tarefaSalvas)) //JSON.parse, transforma o texto no storage em Array novamente para ser exibido na tela//
+    }
+  },[])
+
+  useEffect(() => {
+    if(firstRender.current) {
+      firstRender.current = false
+      return
+    }
+
+    localStorage.setItem("@maxlist", JSON.stringify(tasks))
+  }, [tasks])
+
+
+  const handleSaveEdit = useCallback(() => {
+    const findIndexTask = tasks.findIndex(task => task === editTask.task);
+    const allTasks = [...tasks];
+  
+    allTasks[findIndexTask] = input;
+    setTasks(allTasks);
+  
+    setEditTask({
+      enable: false,
+      task: ''
+    });
+    setInput("");
+  }, [tasks, editTask, input]);
+
+
+
+  const handleRegister = useCallback(() => {
     if (!input) {
       alert("Insira o nome da tarefa!")
       return
@@ -19,24 +56,13 @@ function App() {
       handleSaveEdit()
       return
     }
-
-    setTasks(tarefas =>[...tarefas, input])
+  
+    setTasks(tarefas => [...tarefas, input])
     setInput("")
-  }
+  
+  }, [input,editTask.enable, handleSaveEdit]);
 
-  function handleSaveEdit() {
-    const findIndexTask = tasks.findIndex(task => task === editTask.task)
-    const allTasks = [...tasks]
 
-    allTasks[findIndexTask] = input
-    setTasks(allTasks)
-
-    setEditTask({
-      enable: false,
-      task: ''
-    })
-    setInput("")
-  }
 
   function handleDelete(item: string) {
     const removeTask = tasks.filter(task => task !== item)
@@ -45,6 +71,9 @@ function App() {
   }
 
   function handleEdit(item: string) {
+
+    inputRef.current?.focus()
+
     setInput(item)
     setEditTask({
       enable: true,
@@ -52,12 +81,21 @@ function App() {
     })
   }
 
+
+
+  const totalTarefas = useMemo(() => {
+    return tasks.length
+  }, [tasks])
+
+
   return (
       <div>
         <h1>Lista de Tarefas</h1>
-        <input placeholder="Digite a tarefa..." value={input} onChange={(e) => setInput(e.target.value)} />
+        <input placeholder="Digite a tarefa..." value={input} onChange={(e) => setInput(e.target.value)} ref={inputRef} />
         <button onClick={handleRegister}>{editTask.enable ? "Atualizar tarefa" : "Adicionar tarefa"}</button> 
         <hr />
+        <strong>Você tem {totalTarefas} tarefas!</strong>
+        <br /><br />
 
         {tasks.map((item,) => (
           <section>
